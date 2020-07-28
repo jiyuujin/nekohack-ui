be7a5a1bbbaf8aa816d3a6b616466f9759079f2b
yuma kitamura <yuma.kitamura@smaregi.jp>
2020-07-28 15:10:09 +0900

    built by rollup

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Button from './components/button'
+import Input from './components/input'
+import Label from './components/label'
+import Select from './components/select'
+
+export default { Button, Input, Label, Select }
