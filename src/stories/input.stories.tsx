86a10ca83881a48cf7076d0080fbcbcc4a38c9e0
yuma kitamura <yuma.kitamura@smaregi.jp>
2020-08-18 13:18:01 +0900

    use hooks

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -4,12 +4,12 @@ import * as React from 'react'
 
 import { Input } from '../components/input'
 
-const update = (text: string) => {
-    console.log(text)
-}
-
 storiesOf('Element', module)
-    .add('Text Input', () => <Input value="Input" onChange={update} />)
-    .add('Password Input', () => (
-        <Input value="Input" password onChange={update} />
-    ))
+    .add('Text Input', () => {
+        const [value, setValue] = React.useState('Input')
+        return <Input value={value} onChange={setValue} />
+    })
+    .add('Password Input', () => {
+        const [value, setValue] = React.useState('Input')
+        return <Input value={value} password onChange={setValue} />
+    })
